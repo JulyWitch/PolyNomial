8fe3ea5ef5f9670d0b3eccf5fe025eafe554af54
SajadAbdr <sajad.abdollahi1380@gmail.com>
2020-11-10 23:53:07 +0330

    last

--- Program.cs
+++ Program.cs
@@ -8,22 +8,30 @@ namespace t1_1
     {
         static void Main(string[] args)
         {
-            Poly poly = new Poly(" x-32x^2+x^2+2x^3 -x^2");
-            Poly poly2 = new Poly(" x-32x^2+x^2+2x^3 -x^2");
 
 
-            Poly newn = poly - poly2;
-            Console.WriteLine("MINUX : " + newn.ToString());
-            System.Console.WriteLine("Poly 1 : " + poly.ToString());
-            System.Console.WriteLine("Poly 2 : " + poly2.ToString());
+            while (true)
+            {
+                System.Console.WriteLine("~~~~~ POLY TERM ~~~~~");
+                System.Console.WriteLine("Usage: ");
+                System.Console.WriteLine("     Enter First Polynomial   -> x+2x^2-35x^6");
+                System.Console.WriteLine("     Enter Second Polynomial  -> 5x + 2X^3 - 3 + 28x^2");
 
-            Poly newp = poly + poly2;
-            Console.WriteLine("Sum: " + newp.ToString());
-            System.Console.WriteLine("Poly 1 : " + poly.ToString());
-            System.Console.WriteLine("Poly 2 : " + poly2.ToString());
+                System.Console.WriteLine();
 
+                System.Console.Write("     Enter First Polynomial   -> ");
+                Poly p1 = new Poly("x^3+3 x ^2-4x");
+                System.Console.Write("     Enter Second Polynomial  -> ");
+                Poly p2 = new Poly("-x^2+6x^3");
+                Poly sum = p2 + p1;
+                Poly sub = p1 - p2;
+                System.Console.WriteLine("        Summation : " + sum.ToString());
+                System.Console.WriteLine("        Submission: " + sub.ToString());
+                System.Console.WriteLine(p1.ToString());
+                System.Console.WriteLine(p2.ToString());
+                Console.ReadLine();
 
-            // poly._inputToMember();
+            }
         }
     }
 
@@ -34,7 +42,7 @@ namespace t1_1
         public string input;
         public Poly(string input) : this()
         {
-            this.input = input;
+            this.input = input.ToLower();
             this._inputToMember();
         }
         public Poly(int Length) : this()
@@ -55,33 +63,63 @@ namespace t1_1
         public static Poly operator +(Poly poly, Poly poly2)
         {
 
-            Poly sum = new Poly(poly.members.Length);
+            Poly sum = new Poly(poly.members.Length + poly2.members.Length);
             for (int k = 0; k < poly.members.Length; k++)
             {
                 sum.members[k].power = poly.members[k].power;
                 sum.members[k].prefix = poly.members[k].prefix;
                 sum.members[k].x = poly.members[k].x;
             }
+            for (int k = poly.members.Length; k < poly2.members.Length + poly.members.Length; k++)
+            {
+                sum.members[k].power = poly2.members[k - poly.members.Length].power;
+                sum.members[k].prefix = poly2.members[k - poly.members.Length].prefix;
+                sum.members[k].x = poly2.members[k - poly.members.Length].x;
+            }
+            for (int i = 0; i < sum.members.Length; i++)
+                for (int j = 0; j < sum.members.Length; j++)
+                    if (sum.members[i].power == sum.members[j].power && i != j && sum.members[j].prefix != 0)
+                    {
+                        sum.members[i].prefix += sum.members[j].prefix;
+                        sum.members[j].prefix = 0;
+                        sum.members[j].power = 0;
+                        if (sum.members[i].power == 0) sum.members[i].x = false;
+                    }
+
+
             Poly beSummed = poly2;
-            for (int i = 0; i < poly.members.Length; i++)
-                for (int j = 0; j < beSummed.members.Length; j++)
-                    if (poly.members[i].power == beSummed.members[j].power) sum.members[i].prefix += beSummed.members[j].prefix;
+            // for (int i = 0; i < poly.members.Length; i++)
+            //     for (int j = 0; j < beSummed.members.Length; j++)
+            //         if (poly.members[i].power == beSummed.members[j].power) sum.members[i].prefix += beSummed.members[j].prefix;
             return sum;
         }
 
         public static Poly operator -(Poly poly, Poly poly2)
         {
-            Poly minus = new Poly(poly.members.Length);
+            Poly sub = new Poly(poly.members.Length + poly2.members.Length);
             for (int k = 0; k < poly.members.Length; k++)
             {
-                minus.members[k].power = poly.members[k].power;
-                minus.members[k].prefix = poly.members[k].prefix;
-                minus.members[k].x = poly.members[k].x;
+                sub.members[k].power = poly.members[k].power;
+                sub.members[k].prefix = poly.members[k].prefix;
+                sub.members[k].x = poly.members[k].x;
             }
-            for (int i = 0; i < minus.members.Length; i++)
-                for (int j = 0; j < poly2.members.Length; j++)
-                    if (minus.members[i].power == poly2.members[j].power) minus.members[i].prefix -= poly2.members[j].prefix;
-            return minus;
+            for (int k = poly.members.Length; k < poly2.members.Length + poly.members.Length; k++)
+            {
+                sub.members[k].power = poly2.members[k - poly.members.Length].power;
+                sub.members[k].prefix = -1 * poly2.members[k - poly.members.Length].prefix;
+                sub.members[k].x = poly2.members[k - poly.members.Length].x;
+            }
+            for (int i = 0; i < sub.members.Length; i++)
+                for (int j = 0; j < sub.members.Length; j++)
+                    if (sub.members[i].power == sub.members[j].power && i != j && sub.members[j].prefix != 0)
+                    {
+                        sub.members[i].prefix += sub.members[j].prefix;
+                        sub.members[j].prefix = 0;
+                        sub.members[j].power = 0;
+                        if (sub.members[i].power == 0) sub.members[i].x = false;
+                    }
+
+            return sub;
 
         }
         public void _inputToMember()
@@ -122,13 +160,13 @@ namespace t1_1
             this.x = true;
             this.prefix = 1;
             this.power = 1;
-            if (item[0] == '-')
+            if (item != "" && item[0] == '-')
             {
                 this.prefix *= -1;
                 item = item.Remove(0, 1);
             };
 
-            if (item[0] == '+') item = item.Remove(0, 1);
+            if (item != "" && item[0] == '+') item = item.Remove(0, 1);
             ;
             if (item.Contains("x^"))
             {
@@ -137,8 +175,15 @@ namespace t1_1
             }
             else if (!item.Contains('x'))
             {
+                this.power = 0;
                 this.x = false;
                 this.prefix *= item == "" ? 1 : Convert.ToDouble(item);
+            }
+            else if (item.Contains('x'))
+            {
+                this.power = 1;
+                this.x = true;
+                this.prefix *= item == "x" ? 1 : Convert.ToDouble(item.Replace("x", ""));
             }
             if (item.Contains('^'))
             {
